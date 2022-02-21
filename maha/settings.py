98e729d0571400b9ed073d25026de580be8df66d
rami t wahdan <ramiwahdan1978@gmail.com>
2022-02-21 10:28:34 +0400

    Initial Heroku commit16

--- maha/settings.py
+++ maha/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 MEDIA_URL = 'cart/images/'
