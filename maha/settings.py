0d2800499e8688c3f4273447e0a74d6d069d29f6
rami t wahdan <ramiwahdan1978@gmail.com>
2022-02-22 20:26:59 +0400

    Initial Heroku commit21

--- maha/settings.py
+++ maha/settings.py
@@ -120,17 +120,17 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazoneaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 #AWS_DEFAULT_ACL = ''
 
-AWS_LOCATION = '/static/'
+AWS_LOCATION = 'static/'
 STATICFILES_DIRS = [
 
-    os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 MEDIA_URL = '/cart/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/static/cart/images/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/cart/images/')
 
 #email local settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
