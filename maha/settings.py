b747ea3c1d01bfd13998057b48ec5a07af04b8e6
rami t wahdan <ramiwahdan1978@gmail.com>
2022-02-23 11:54:50 +0400

    Initial Heroku commit301

--- maha/settings.py
+++ maha/settings.py
@@ -122,7 +122,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazoneaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 #AWS_DEFAULT_ACL = ''
 
-AWS_LOCATION = 'arn:aws:s3:::maha2022/static/'
+AWS_LOCATION = 'static/'
 STATICFILES_DIRS = [
 
     os.path.join(BASE_DIR, 'static/'),
