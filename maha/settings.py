902c2b9b82ff2d014220838b465e74867aa88abb
rami t wahdan <ramiwahdan1978@gmail.com>
2022-02-25 11:56:52 +0400

    Initial Heroku commit302

--- maha/settings.py
+++ maha/settings.py
@@ -119,13 +119,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = [
+STATICFILES_DIRS = [
 
-#    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 
-#]
+]
 
 #MEDIA_URL = 'cart/images/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'static/cart/images')
