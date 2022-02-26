37ef7f90ce639a97ae9e1cec4f691a8087b7affd
rami t wahdan <ramiwahdan1978@gmail.com>
2022-02-26 20:34:01 +0400

    Initial Heroku commit302

--- maha/aws/conf.py
+++ maha/aws/conf.py
@@ -10,4 +10,4 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-DEFAULT_FILE_STORAGE = 'maha.settings.aws.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
