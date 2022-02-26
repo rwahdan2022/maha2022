1cf84b888e6e30d515e9906b9460ca991b0fca95
rami t wahdan <ramiwahdan1978@gmail.com>
2022-02-26 20:51:48 +0400

    Initial Heroku commit302

--- maha/aws/conf.py
+++ maha/aws/conf.py
@@ -1,4 +1,4 @@
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = '{cart}.settings.aws.storage_backends.MediaStorage'
 
 AWS_ACCESS_KEY_ID = 'AKIAV6AB5EJOASMYI555'
 AWS_SECRET_ACCESS_KEY = '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'
@@ -12,3 +12,4 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
