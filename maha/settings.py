28f6d3564fbb0efec4146aae37f5ef5845b50eac
rami wahdan <ramitaherwahdan1978@gmail.com>
2022-03-07 10:51:05 +0400

    Initial Heroku commit401

--- maha/settings.py
+++ maha/settings.py
@@ -114,6 +114,23 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# cfehome/settings.py
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+LINODE_BUCKET=os.environ.get('LINODE_BUCKET', 'cfe2')
+LINODE_BUCKET_REGION=os.environ.get('LINODE_BUCKET_REGION',  'ap-south-1')
+LINODE_BUCKET_ACCESS_KEY=os.environ.get('LINODE_BUCKET_ACCESS_KEY', 'N2Y7396FB2M72IPBBJN9') 
+LINODE_BUCKET_SECRET_KEY=os.environ.get('LINODE_BUCKET_SECRET_KEY', 'Tdgg7NilAv298ZFtLawlojmuc5jHIumq3fIRySUl') 
+
+AWS_S3_ENDPOINT_URL=f'https://{LINODE_BUCKET_REGION}.linodeobjects.com'
+AWS_ACCESS_KEY_ID=LINODE_BUCKET_ACCESS_KEY
+AWS_SECRET_ACCESS_KEY=LINODE_BUCKET_SECRET_KEY
+AWS_S3_REGION_NAME=LINODE_BUCKET_REGION
+AWS_S3_USE_SSL=True
+AWS_STORAGE_BUCKET_NAME=LINODE_BUCKET
+
 #email local settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
