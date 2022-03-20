7160fe900f73a8f8cf7ec868b2139e099a6c7794
rami wahdan <ramitaherwahdan1978@gmail.com>
2022-03-20 08:06:41 +0400

    Initial Heroku commit_final3

--- cart/views.py
+++ cart/views.py
@@ -100,7 +100,7 @@ def transactions(request,order):
 			order_id = order.order
 			order_date = order.order.date_ordered
 			thecustomer = order.order.customer
-			theimage = "https://cfe2.ap-south-1.linodeobjects.com/receipts/"+order.order.image
+			theimage = order.order.image
 			thequantity = order.quantity
 			customer_email = order.order.customer.email
 			transaction_id = order.order.transaction_id
