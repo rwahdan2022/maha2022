19ce8a40c5b6f9f01b791cf530eba66b7fcb5518
rami wahdan <ramitaherwahdan1978@gmail.com>
2022-03-22 13:24:41 +0400

    Initial Heroku commit_final3

--- maha/settings.py
+++ maha/settings.py
@@ -150,7 +150,7 @@ EMAIL_HOST_USER = 'ramifamilyphotos2018@gmail.com'
 EMAIL_HOST_PASSWORD='ijzpiqeqjubfpqvb'
 
 
-SESSION_EXPIRE_SECONDS = 300                # 5minutes
-SESSION_COOKIE_AGE = 5                      # 1hour
+SESSION_EXPIRE_SECONDS = 300                # 5 minutes
+SESSION_COOKIE_AGE = 300                    # 5 minutes
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True      # clear on close
                   
